226e66aa6e781cf16d2f1e14b3cd78349015e634
Chiara Aengevelt <chiara@aengevelt.com>
2021-01-14 19:17:29 +0100

    MVP Routes all added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroy restaurants - clean database"
+Restaurant.destroy_all
+puts "Database cleared"
+
+5.times do
+    restaurant =Restaurant.create(
+        name: Faker::Restaurant.name, 
+        address: Faker::Address.city,
+        category: ["chinese", "italian", "japanese", "french", "belgian"].sample, 
+        phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    )
+    puts "restaurant #{restaurant.id}"
+end
